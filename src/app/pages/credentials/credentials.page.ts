2bd672fa5a4923942cbbd52349f7aec452121807
Roger Miret <roger.miret@in2.es>
2026-02-12 15:25:39 +0100

    credentials: temporarily call oid4vci engine when scanning credential offer url

--- src/app/pages/credentials/credentials.page.ts
+++ src/app/pages/credentials/credentials.page.ts
@@ -141,18 +141,30 @@ export class CredentialsPage implements OnInit, ViewWillLeave {
   }
 
   public qrCodeEmit(qrCode: string): void {
+    let executeContentSucessCallback: (arg: any) => Observable<any>;
     const isCredentialOffer = qrCode.includes('credential_offer_uri');
     //todo don't accept qrs that are not to login or get VC
     if(isCredentialOffer){
       //show VCs list
       this.closeScannerViewAndScanner();
-
+      // CROSS-DEVICE CREDENTIAL OFFER FLOW
+      executeContentSucessCallback = () => {   
+        return this.handleActivationSuccess();
+      }
     }else{
       // LOGIN / VERIFIABLE PRESENTATION
       // hide scanner but don't show VCs list
       this.closeScanner();
       this.loader.addLoadingProcess();
-
+      executeContentSucessCallback = (executionResponse: JSON) => {
+        return from(this.router.navigate(['/tabs/vc-selector/'], {
+          queryParams: {
+            executionResponse: JSON.stringify(executionResponse),
+          },
+        })).pipe(
+          tap(() => { this.loader.removeLoadingProcess() })
+        );
+      }
     }
     const socketsToConnect: Promise<void>[] = [];
     if (isCredentialOffer) socketsToConnect.push(this.websocket.connectNotificationSocket());
@@ -161,8 +173,11 @@ export class CredentialsPage implements OnInit, ViewWillLeave {
       .pipe(
         switchMap(() => {
           this.loader.addLoadingProcess();
+          if(isCredentialOffer){
+
+            return this.oid4vciEngineService.executeOid4vciFlow(qrCode);
+          }
           return this.walletService.executeContent(qrCode);
-          return this.oid4vciEngineService.executeOid4vciFlow(qrCode);
         }),
         switchMap((executionResponse) => {
           if (isCredentialOffer) {
@@ -201,7 +216,8 @@ export class CredentialsPage implements OnInit, ViewWillLeave {
         }),
 
         switchMap(() =>
-          this.walletService.requestOpenidCredentialOffer(this.credentialOfferUri)
+          // this.walletService.requestOpenidCredentialOffer(this.credentialOfferUri)
+        this.oid4vciEngineService.executeOid4vciFlow(this.credentialOfferUri)
         ),
 
         switchMap(() => this.handleActivationSuccess()),
